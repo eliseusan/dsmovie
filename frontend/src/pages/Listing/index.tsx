279057b88d90354c1dd13f783188345c04d65962
Eliseu <eliseusantico@gmail.com>
2022-03-15 21:15:04 -0300

    Incremento usando Props e voltei a versao anterior a essa

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,58 +4,59 @@ import { useEffect, useState } from "react";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
-import {MoviePage } from "types/movie";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+    useEffect(() => {
 
-    useEffect(() =>{
-
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
-        .then(response =>{
-            const data = response.data as MoviePage;
-            console.log(data);
-            setPageNumber(data.number);
-
-
-        }) ;
-
-    }, []);
-
- /*forma errada 
-    axios.get(`${BASE_URL}/movies?size=12&page=1`)
-        .then(response =>{
-            const data = response.data as MoviePage;
-            setPageNumber(data.number);
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data)
 
 
-        }) ;*/
+
+            });
+
+    }, [pageNumber]);
+
+    /*forma errada 
+       axios.get(`${BASE_URL}/movies?size=12&page=1`)
+           .then(response =>{
+               const data = response.data as MoviePage;
+               setPageNumber(data.number);
+   
+   
+           }) ;*/
 
     return (
         <>
 
-            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
 
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 mb-3"  >
-                        <MovieCard />
-                    </div>
 
-                    <div className="col-sm-6 col-lg-4 mb-3"  >
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 mb-3"  >
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
 
-                    <div className="col-sm-6 col-lg-4 mb-3"  >
-                        <MovieCard />
-                    </div>
-
-                    <div className="col-sm-6 col-lg-4 mb-3"  >
-                        <MovieCard />
-                    </div>
-                    
                 </div>
 
             </div>
